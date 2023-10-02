7ac1f25722636f9519cefe0be88aac5b2ccea0ee
jesus_athf <michischaetzle@web.de>
2023-10-02 13:00:25 -0400

    cloudant database connections done

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -1,30 +1,63 @@
 /**
  * Get all databases
  */
-
- const { CloudantV1 } = require("@ibm-cloud/cloudant");
- const { IamAuthenticator } = require("ibm-cloud-sdk-core");
- 
- function main(params) {
-   const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
-   const cloudant = CloudantV1.newInstance({
-     authenticator: authenticator,
-   });
-   cloudant.setServiceUrl(params.COUCH_URL);
- 
-   let dbList = getDbs(cloudant);
-   return { dbs: dbList };
- }
- 
- function getDbs(cloudant) {
-   cloudant
-     .getAllDbs()
-     .then((body) => {
-       body.forEach((db) => {
-         dbList.push(db);
-       });
-     })
-     .catch((err) => {
-       console.log(err);
-     });
- }
+async function main(params) {
+    const { CloudantV1 } = require('@ibm-cloud/cloudant');
+    const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+    const authenticator = new IamAuthenticator({ apikey: 'vwihyHQ5ekJRB02JUarrC1c8H8CSYDHax-5s9Ymta-mE' })
+    const cloudant = CloudantV1.newInstance({
+      authenticator: authenticator
+    });
+    cloudant.setServiceUrl('https://apikey-v2-k1xhitwcmkebvzelp7z21l1x7zsnewn5cykl50ilk8t:56bf52d0826be71246039afa3a46eb19@542d5355-caae-474a-9aba-7ee4a461f0b5-bluemix.cloudantnosqldb.appdomain.cloud');
+  
+    try {
+      if (params.st) {
+        const result = await findDealershipByState(cloudant, params.st);
+        const code = result.length ? 200 : 404;
+        return {
+          statusCode: code,
+          headers: { 'Content-Type': 'application/json' },
+          body: result
+        };
+      } else if (params.id) {
+        const result = await findDealershipById(cloudant, params.id);
+        const code = result.length ? 200 : 404;
+        return {
+          statusCode: code,
+          headers: { 'Content-Type': 'application/json' },
+          body: result
+        };
+      } else {
+        const result = await findAllDealerships(cloudant);
+        const code = result.length ? 200 : 404;
+        return {
+          statusCode: code,
+          headers: { 'Content-Type': 'application/json' },
+          body: result
+        };
+      }
+    } catch (err) {
+      console.error(err);
+      return {
+        statusCode: 500,
+        headers: { 'Content-Type': 'application/json' },
+        body: { error: 'Internal Server Error' }
+      };
+    }
+  }
+  
+  async function findDealershipByState(cloudant, state) {
+    const result = await cloudant.postFind({ db: 'dealerships', selector: { st: state } });
+    return result.result.docs;
+  }
+  
+  async function findDealershipById(cloudant, id) {
+    const result = await cloudant.postFind({ db: 'dealerships', selector: { id: parseInt(id) } });
+    return result.result.docs;
+  }
+  
+  async function findAllDealerships(cloudant) {
+    const result = await cloudant.postAllDocs({ db: 'dealerships', includeDocs: true, limit: 10 });
+    return result.result.rows;
+  }
+  
--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -1,35 +1,46 @@
 """IBM Cloud Function that gets all reviews for a dealership
-
 Returns:
     List: List of reviews for the given dealership
 """
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
-import requests
+import sys 
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-
-def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
-
+def get_review(dict): 
+    authenticator = IAMAuthenticator("vwihyHQ5ekJRB02JUarrC1c8H8CSYDHax-5s9Ymta-mE")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://apikey-v2-k1xhitwcmkebvzelp7z21l1x7zsnewn5cykl50ilk8t:56bf52d0826be71246039afa3a46eb19@542d5355-caae-474a-9aba-7ee4a461f0b5-bluemix.cloudantnosqldb.appdomain.cloud")
+    response = service.post_find(
+                db='reviews',
+                selector={'dealership': {'$eq': int(dict['id'])}},
+            ).get_result()
+    try: 
+        # result_by_filter=my_database.get_query_result(selector,raw_result=True) 
+        result= {
+            'headers': {'Content-Type':'application/json'}, 
+            'body': {'data':response} 
+            }        
+        return result
+    except:  
+        return { 
+            'statusCode': 404, 
+            'message': 'Something went wrong'
+            }
+"""Posts a new review into an existing database"""
+def post_review(dict):
+    authenticator = IAMAuthenticator("vwihyHQ5ekJRB02JUarrC1c8H8CSYDHax-5s9Ymta-mE")
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://apikey-v2-k1xhitwcmkebvzelp7z21l1x7zsnewn5cykl50ilk8t:56bf52d0826be71246039afa3a46eb19@542d5355-caae-474a-9aba-7ee4a461f0b5-bluemix.cloudantnosqldb.appdomain.cloud")
+    response = service.post_document(db='reviews', document=dict["review"]).get_result()
     try:
-        client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"dbs": client.all_dbs()}
+    # result_by_filter=my_database.get_query_result(selector,raw_result=True)
+        result= {
+        'headers': {'Content-Type':'application/json'},
+        'body': {'data':response}
+        }
+        return result
+    except:
+        return {
+        'statusCode': 404,
+        'message': 'Something went wrong'
+        }
